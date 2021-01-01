53e415a51872ce7ce91c479ed87fe2f29c4a6d7a
Kimberly Yang <kimberlytyang@gmail.com>
2020-12-31 16:25:39 -0800

    Refactored with a coordinate system and speed limiter

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pygame
 
+# SETTINGS
 BLOCK_SIZE = 18  # Board Block Size
 BLOCK_HORIZONTAL = 28  # Board Width in Blocks
 BLOCK_VERTICAL = 20  # Board Height in Blocks
 FONT_SIZE = 40  # Score Text Size
-BOARD_REF_X = 30  # Border
-BOARD_REF_Y = 30  # Border
+BORDER = 30  # Border Around Game Board
 
 BOARD_WIDTH = BLOCK_HORIZONTAL * BLOCK_SIZE
 BOARD_HEIGHT = BLOCK_VERTICAL * BLOCK_SIZE
 
-WIDTH = BOARD_WIDTH + 2 * BOARD_REF_X
-HEIGHT = BOARD_HEIGHT + 2 * BOARD_REF_Y + FONT_SIZE
+WIDTH = BOARD_WIDTH + 2 * BORDER
+HEIGHT = BOARD_HEIGHT + 2 * BORDER + FONT_SIZE
 
 WINDOW_COLOR = [160, 173, 158]
 
@@ -33,50 +33,63 @@ class LinkedList:
         self.tail = None
 
 
+def pixels(coord):
+    horizontal = coord[0] * BLOCK_SIZE + BORDER
+    vertical = coord[1] * BLOCK_SIZE + BORDER
+    return [horizontal, vertical]
+
+
 class Snake:
     def __init__(self, start_x, start_y):
         self.path = LinkedList()
         self.path.head = Node([start_x, start_y])
         self.path.tail = self.path.head
-        self.cherry = [start_x + 100, start_y + 100]
-        self.extend = 5
+        self.length = 0
+        self.cherry = [start_x + 5, start_y + 5]
+        self.extend = 10
 
     def start(self):
-        pygame.draw.rect(window, (0, 0, 0), (self.path.tail.data[0], self.path.tail.data[1], BLOCK_SIZE, BLOCK_SIZE))
+        convert = pixels(self.path.tail.data)
+        pygame.draw.rect(window, (0, 0, 0), (convert[0], convert[1], BLOCK_SIZE, BLOCK_SIZE))
 
     def move(self, direction):
+        coord = self.path.tail.data
         if direction == "U":
-            self.path.tail.next = Node([self.path.tail.data[0], self.path.tail.data[1] - BLOCK_SIZE])
+            self.path.tail.next = Node([coord[0], coord[1] - 1])
             self.path.tail = self.path.tail.next
         elif direction == "D":
-            self.path.tail.next = Node([self.path.tail.data[0], self.path.tail.data[1] + BLOCK_SIZE])
+            self.path.tail.next = Node([coord[0], coord[1] + 1])
             self.path.tail = self.path.tail.next
         elif direction == "L":
-            self.path.tail.next = Node([self.path.tail.data[0] - BLOCK_SIZE, self.path.tail.data[1]])
+            self.path.tail.next = Node([coord[0] - 1, coord[1]])
             self.path.tail = self.path.tail.next
         elif direction == "R":
-            self.path.tail.next = Node([self.path.tail.data[0] + BLOCK_SIZE, self.path.tail.data[1]])
+            self.path.tail.next = Node([coord[0] + 1, coord[1]])
             self.path.tail = self.path.tail.next
         else:
             print("Invalid Direction")
 
-        if self.path.tail.data[0] == self.cherry[0] and self.path.tail.data[1] == self.cherry[1]:
+        coord = self.path.tail.data
+        coord_cherry = self.cherry
+        if coord[0] == coord_cherry[0] and coord[1] == coord_cherry[1]:
             self.eat()
 
         if self.extend == 0:
-            # temp = self.path.head                # Free up memory?
-            self.path.head = self.path.head.next
-            # temp = None
+            temp = self.path.head.next
+            self.path.head = None
+            self.path.head = temp
         else:
             self.extend -= 1
 
         curr = self.path.head
         while curr is not None:
-            pygame.draw.rect(window, (0, 0, 0), (curr.data[0], curr.data[1], BLOCK_SIZE, BLOCK_SIZE))
+            convert = pixels(curr.data)
+            pygame.draw.rect(window, (0, 0, 0), (convert[0], convert[1], BLOCK_SIZE, BLOCK_SIZE))
             curr = curr.next
 
     def eat(self):
-        self.extend += 3
+        self.extend += 1
+        self.length += 1
 
     def random_spawn(self):
         x_coord
@@ -86,26 +99,26 @@ class Snake:
 def draw_screen(score):
     window.fill(WINDOW_COLOR)
     score_display = text.render("Score: " + str(score), 1, (0, 0, 0))
-    window.blit(score_display, (BOARD_REF_X, HEIGHT - (BOARD_REF_Y / 2) - FONT_SIZE))
+    window.blit(score_display, (BORDER, HEIGHT - (BORDER / 2) - FONT_SIZE))
 
 
 def draw_grid():
     num_blocks_vertical = int(BOARD_HEIGHT / BLOCK_SIZE)
     num_blocks_horizontal = int(BOARD_WIDTH / BLOCK_SIZE)
     for i in range(num_blocks_vertical - 1):
-        pygame.draw.line(window, (139, 150, 137), (BOARD_REF_X, BOARD_REF_Y + BLOCK_SIZE * (i + 1)), (BOARD_REF_X + BOARD_WIDTH, BOARD_REF_Y + BLOCK_SIZE * (i + 1)))
+        pygame.draw.line(window, (139, 150, 137), (BORDER, BORDER + BLOCK_SIZE * (i + 1)), (BORDER + BOARD_WIDTH, BORDER + BLOCK_SIZE * (i + 1)))
     for i in range(num_blocks_horizontal - 1):
-        pygame.draw.line(window, (139, 150, 137), (BOARD_REF_X + BLOCK_SIZE * (i + 1), BOARD_REF_Y), (BOARD_REF_X + BLOCK_SIZE * (i + 1), BOARD_REF_Y + BOARD_HEIGHT))
-    pygame.draw.rect(window, (0, 0, 0), (BOARD_REF_X, BOARD_REF_Y, BOARD_WIDTH, BOARD_HEIGHT), 1)
+        pygame.draw.line(window, (139, 150, 137), (BORDER + BLOCK_SIZE * (i + 1), BORDER), (BORDER + BLOCK_SIZE * (i + 1), BORDER + BOARD_HEIGHT))
+    pygame.draw.rect(window, (0, 0, 0), (BORDER, BORDER, BOARD_WIDTH, BOARD_HEIGHT), 1)
 
 
 def main():
     run = True
-    fps = 10
+    fps = 60
     score = 0
     clock = pygame.time.Clock()
 
-    snake = Snake(BOARD_REF_X + BLOCK_SIZE, BOARD_REF_Y + BLOCK_SIZE)
+    snake = Snake(1, 1)
     snake.start()
 
     global direction
@@ -146,6 +159,7 @@ def main():
         draw_grid()
         pygame.display.update()
 
+    frame_limiter = 4
     while run:
         clock.tick(fps)
 
@@ -165,10 +179,14 @@ def main():
                 elif event.key == pygame.K_RIGHT and direction != "L":
                     direction = "R"
                     print(direction)
-        draw_screen(score)
-        snake.move(direction)
-        draw_grid()
-        pygame.display.update()
+        if frame_limiter > 0:
+            frame_limiter -= 1
+        else:
+            frame_limiter = 4
+            draw_screen(score)
+            snake.move(direction)
+            draw_grid()
+            pygame.display.update()
 
 
 main()
